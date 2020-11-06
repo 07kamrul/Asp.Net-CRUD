d34a928a59adc147d8db414424c695b10fd65da8
07kamrul <mdkamrulhasanewu@gmail.com>
2020-11-07 02:53:33 +0600

    Asp Dot Net CRUD project

---
+++ Full CRUD/Models/Extended/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Full_CRUD.Models.Extended
+{
+    [MetadataType(typeof(EmployeeMetadata))]
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMetadata 
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please provice first name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provice last name")]
+        public string LastName { get; set; }
+    }
+}
